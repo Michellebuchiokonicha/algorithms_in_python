986ee0bea995b95342fc2dadd83f5b9844ea6f0a
michellebuchiokonicha <michelleokonicha.com>
2024-01-11 13:09:32 +0100

    u

---
+++ group_anagrams.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = {} #mapping charCount to list of Anagrams
+
+        for s in strs:
+            count = [0] * 26 #a ... z
+            
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            res[count].append(s)
+
+        return res.values()
+    
+        O(m * n)
